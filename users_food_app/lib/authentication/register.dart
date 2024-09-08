6bcb60939ffc0f775b2305e6b12c7f3efe319712
yazhou <wwwqinyazhou@163.com>
2024-09-09 00:02:57 +0900

    fix firebase issue

--- users_food_app/lib/authentication/register.dart
+++ users_food_app/lib/authentication/register.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -9,7 +10,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as fStorage;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../authentication/login.dart';
-
 import '../global/global.dart';
 import '../screens/home_screen.dart';
 import '../widgets/custom_text_field.dart';
@@ -31,97 +31,95 @@ class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmpasswordController = TextEditingController();
 
-//image picker
+  // Image picker
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
 
-//seller image url
+  // User image URL
   String userImageUrl = "";
 
-//function for getting image
+  // Function to get image (web and mobile)
   Future<void> _getImage() async {
-    imageXFile = await _picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      imageXFile;
-    });
+    if (kIsWeb) {
+      final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        final Uint8List imageData = await pickedFile.readAsBytes();
+        setState(() {
+          imageXFile = XFile.fromData(imageData, name: pickedFile.name);
+        });
+      }
+    } else {
+      imageXFile = await _picker.pickImage(source: ImageSource.gallery);
+      setState(() {});
+    }
   }
 
-//Form Validation
+  // Form validation
   Future<void> signUpFormValidation() async {
-    //checking if user selected image
     if (imageXFile == null) {
-      setState(
-        () {
-          // imageXFile == "images/bg.png";
-          showDialog(
-            context: context,
-            builder: (c) {
-              return const ErrorDialog(
-                message: "Please select an image",
-              );
-            },
-          );
+      showDialog(
+        context: context,
+        builder: (c) {
+          return const ErrorDialog(message: "Please select an image");
         },
       );
-    } else {
-      if (passwordController.text == confirmpasswordController.text) {
-        //nested if (cheking if controllers empty or not)
-        if (confirmpasswordController.text.isNotEmpty &&
-            emailController.text.isNotEmpty &&
-            nameController.text.isNotEmpty) {
-          //start uploading image
-          showDialog(
-            context: context,
-            builder: (c) {
-              return const LoadingDialog(
-                message: "Registering Account",
-              );
-            },
-          );
+    } else if (passwordController.text == confirmpasswordController.text) {
+      if (nameController.text.isNotEmpty &&
+          emailController.text.isNotEmpty &&
+          passwordController.text.isNotEmpty) {
+        
+        showDialog(
+          context: context,
+          builder: (c) {
+            return const LoadingDialog(message: "Registering Account");
+          },
+        );
 
-          String fileName = DateTime.now().millisecondsSinceEpoch.toString();
-          fStorage.Reference reference = fStorage.FirebaseStorage.instance
-              .ref()
-              .child("users")
-              .child(fileName);
+        String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+        fStorage.Reference reference = fStorage.FirebaseStorage.instance
+            .ref()
+            .child("users")
+            .child(fileName);
+
+        if (kIsWeb) {
+          // Web upload using data from XFile
+          final Uint8List imageData = await imageXFile!.readAsBytes();
+          fStorage.UploadTask uploadTask = reference.putData(imageData);
+          fStorage.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+          await taskSnapshot.ref.getDownloadURL().then((url) {
+            userImageUrl = url;
+            AuthenticateSellerAndSignUp();
+          });
+        } else {
+          // Mobile upload
           fStorage.UploadTask uploadTask =
               reference.putFile(File(imageXFile!.path));
           fStorage.TaskSnapshot taskSnapshot =
               await uploadTask.whenComplete(() {});
           await taskSnapshot.ref.getDownloadURL().then((url) {
             userImageUrl = url;
-
-            // save info to firestore
             AuthenticateSellerAndSignUp();
           });
         }
-        //if there is empty place show this message
-        else {
-          showDialog(
-            context: context,
-            builder: (c) {
-              return const ErrorDialog(
-                message: "Please fill the required info for Registration. ",
-              );
-            },
-          );
-        }
       } else {
-        //show an error if passwords do not match
         showDialog(
           context: context,
           builder: (c) {
-            return const ErrorDialog(
-              message: "Password do not match",
-            );
+            return const ErrorDialog(message: "Please fill in all fields");
           },
         );
       }
+    } else {
+      showDialog(
+        context: context,
+        builder: (c) {
+          return const ErrorDialog(message: "Passwords do not match");
+        },
+      );
     }
   }
 
-  // ignore: non_constant_identifier_names
+  // Authenticate user and sign up
   void AuthenticateSellerAndSignUp() async {
     User? currentUser;
     await firebaseAuth
@@ -148,14 +146,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
     if (currentUser != null) {
       saveDataToFirestore(currentUser!).then((value) {
         Navigator.pop(context);
-        //send user to Home Screen
+        // Navigate to Home Screen
         Route newRoute = MaterialPageRoute(builder: (c) => const HomeScreen());
         Navigator.pushReplacement(context, newRoute);
       });
     }
   }
 
-//saving seller information to firestore
+  // Save user data to Firestore
   Future saveDataToFirestore(User currentUser) async {
     FirebaseFirestore.instance.collection("users").doc(currentUser.uid).set(
       {
@@ -168,14 +166,22 @@ class _RegisterScreenState extends State<RegisterScreen> {
       },
     );
 
-    // save data locally (to access data easly from phone storage)
+    // Save data locally
     sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences!.setString("uid", currentUser.uid);
     await sharedPreferences!.setString("email", currentUser.email.toString());
     await sharedPreferences!.setString("name", nameController.text.trim());
     await sharedPreferences!.setString("photoUrl", userImageUrl);
     await sharedPreferences!.setStringList(
-        "userCart", ['garbageValue']); //empty cart list while registration
+        "userCart", ['garbageValue']); // Empty cart list on registration
+  }
+
+  // Function to get image data
+  Future<Uint8List?> getImageData() async {
+    if (imageXFile != null) {
+      return await imageXFile!.readAsBytes();
+    }
+    return null;
   }
 
   @override
@@ -225,21 +231,36 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       onTap: () {
                         _getImage();
                       },
-                      child: CircleAvatar(
-                        radius: MediaQuery.of(context).size.width * 0.20,
-                        backgroundColor: Colors.white,
-                        backgroundImage: imageXFile == null
-                            ? null
-                            : FileImage(
-                                File(imageXFile!.path),
-                              ),
-                        child: imageXFile == null
-                            ? Icon(
-                                Icons.person_add_alt_1,
-                                size: MediaQuery.of(context).size.width * 0.20,
-                                color: Colors.grey,
-                              )
-                            : null,
+                      child: FutureBuilder<Uint8List?>(
+                        future: getImageData(),
+                        builder: (context, snapshot) {
+                          ImageProvider<Object>? imageProvider;
+                          
+                          if (snapshot.connectionState == ConnectionState.done) {
+                            if (snapshot.data != null) {
+                              if (kIsWeb) {
+                                // For web, create a MemoryImage
+                                imageProvider = MemoryImage(snapshot.data!);
+                              } else {
+                                // For mobile, create a FileImage
+                                imageProvider = FileImage(File(imageXFile!.path));
+                              }
+                            }
+                          }
+
+                          return CircleAvatar(
+                            radius: MediaQuery.of(context).size.width * 0.20,
+                            backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+                            backgroundImage: imageProvider,
+                            child: imageXFile == null
+                                ? Icon(
+                                    Icons.person_add_alt_1,
+                                    size: MediaQuery.of(context).size.width * 0.20,
+                                    color: Colors.grey,
+                                  )
+                                : null,
+                          );
+                        },
                       ),
                     ),
                   ),
@@ -271,87 +292,28 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     CustomTextField(
                       data: Icons.lock,
                       controller: confirmpasswordController,
-                      hintText: "Confirm password",
+                      hintText: "Confirm Password",
                       isObsecre: true,
                     ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        signUpFormValidation();
+                      },
+                      child: const Text("Sign Up"),
+                    ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()));
+                      },
+                      child: const Text("Already have an account? Login"),
+                    ),
+                    const SizedBox(height: 10),
                   ],
-                ),
-              ),
-              const SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 30),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0, 4),
-                          blurRadius: 5.0)
-                    ],
-                    gradient: const LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      stops: [0.0, 1.0],
-                      colors: [
-                        Colors.amber,
-                        Colors.black,
-                      ],
-                    ),
-                    color: Colors.deepPurple.shade300,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                      ),
-                      minimumSize:
-                          MaterialStateProperty.all(const Size(50, 50)),
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                      shadowColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                      child: Text(
-                        'Sign Up'.toUpperCase(),
-                        style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                    onPressed: () {
-                      signUpFormValidation();
-                    },
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      const TextSpan(text: "Already have an account? "),
-                      TextSpan(
-                        text: 'Login',
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const LoginScreen()));
-                          },
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
               ),
             ],
